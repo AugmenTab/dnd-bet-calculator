c243c74f1f4ea259c80950fff8cad8717df9961d
AugmenTab <tbaum92@live.com>
2020-07-10 19:38:49 -0600

    Completed setup function.

--- calculator.py
+++ calculator.py
@@ -1,7 +1,16 @@
 #! python3
 
-def programStart():
+def calculateWinnings(odds, wager):
     pass
+
+def programStart():
+    odds = eval(input('What are the odds?\n')) + 1
+    ppWager = int(input('How many pp on the bet?\n'))
+    gpWager = int(input('How many gp on the bet?\n'))
+    spWager = int(input('How many sp on the bet?\n'))
+    cpWager = int(input('How many cp on the bet?\n'))
+    wager = (ppWager * 1000) + (gpWager * 100) + (spWager * 10) + cpWager
+    calculateWinnings(odds, wager)
 
 if __name__ == "__main__":
     programStart()
